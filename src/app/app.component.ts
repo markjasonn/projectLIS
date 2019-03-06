1aa295e8d9c99693ff2aeca0703f6ae2760607d5
jeraldjohn <34527907+jeraldjohngalicha@users.noreply.github.com>
2019-03-06 14:29:01 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'LIS-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'projectLIS';
+}
