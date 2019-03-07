05f14f15959d639e833fc0cd4ac7792d9324e369
jeromenciel <45193320+jeromenciel@users.noreply.github.com>
2019-03-07 15:48:09 +0800

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule, MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
@@ -16,5 +22,8 @@ import { AppComponent } from './app.component';
 export class AppModule {
   constructor() {
     console.log('sample');
+
+
+    //jkljklj
   }
  }
