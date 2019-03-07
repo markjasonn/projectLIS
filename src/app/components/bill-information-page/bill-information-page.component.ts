2ec11ab0bcc868bfd4046ac32c4e5223aac1737a
jeraldjohn <34527907+jeraldjohngalicha@users.noreply.github.com>
2019-03-07 15:37:32 +0800

    Added template components

---
+++ src/app/components/bill-information-page/bill-information-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'LIS-bill-information-page',
+  templateUrl: './bill-information-page.component.html',
+  styleUrls: ['./bill-information-page.component.scss']
+})
+export class BillInformationPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
