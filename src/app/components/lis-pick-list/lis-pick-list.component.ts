8a3132034f70b3bbc285220432e502d982eedd52
jeraldjohn <34527907+jeraldjohngalicha@users.noreply.github.com>
2019-03-13 16:04:32 +0800

    Added functionality for PickList in Search

--- src/app/components/lis-pick-list/lis-pick-list.component.ts
+++ src/app/components/lis-pick-list/lis-pick-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'LIS-lis-pick-list',
@@ -8,8 +8,16 @@ import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/cor
 export class LisPickListComponent implements OnInit, OnChanges {
 
   @Input() sourceList:any [];
-  private currentSourceList: any[];
-  private destinationList: any[];
+  @Output() okButtonFunction: EventEmitter<any>;
+
+  @ViewChild("sourceList") sourceListEl: ElementRef;
+  
+
+  private currentSourceList: any[] = new Array();
+  private selectedList: any[] = new Array();
+  private sourceSelectedItem: any[] = new Array();
+  private destSelectedItem: any[] = new Array();
+  
 
 
   constructor() { }
@@ -24,6 +32,30 @@ export class LisPickListComponent implements OnInit, OnChanges {
       this.currentSourceList = this.sourceList;
     }
 
+  }
+
+  moveSelected(): void {
+    for(let selected of this.sourceSelectedItem){
+      this.currentSourceList.splice(this.currentSourceList.indexOf(selected), 1);
+      this.selectedList.push(selected);
+    }
+  }
+
+  moveAll(): void {
+    this.selectedList.push.apply(this.selectedList, this.currentSourceList);
+    this.currentSourceList = [];
+  }
+
+  returnSelected(): void {
+    for(let selected of this.destSelectedItem){
+      this.selectedList.splice(this.selectedList.indexOf(selected), 1);
+      this.currentSourceList.push(selected);
+    }
+  }
+
+  returnAll(): void {
+    this.currentSourceList.push.apply(this.currentSourceList, this.selectedList);
+    this.selectedList = [];
   }
 
 }
