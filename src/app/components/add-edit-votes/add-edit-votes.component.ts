861d8616d03c9c6df572a20610836cc2e1ba4df9
jeraldjohn <34527907+jeraldjohngalicha@users.noreply.github.com>
2019-03-15 11:11:54 +0800

    For production build command

---
+++ src/app/components/add-edit-votes/add-edit-votes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'LIS-add-edit-votes',
+  templateUrl: './add-edit-votes.component.html',
+  styleUrls: ['./add-edit-votes.component.scss']
+})
+export class AddEditVotesComponent implements OnInit {
+
+  private senatorVoteStatus: SenatorVoteStatus[] = [
+    {name:'ANGARA, JUAN EDUARDO "SONNY" M.', yes:'Yes', no:'', abstain:''},
+    {name:'AQUINO IV, PAULO BENIGNO "BAM".', yes:'', no:'No', abstain:''},
+    {name:'BINAY, MARIA LOURDES NANCY S.', yes:'', no:'NO', abstain:''},
+    {name:'CAYETANO, ALAN PETER COMPAÑERO S.', yes:'Yes', no:'', abstain:''},
+    {name:'DE LIMA, LEILA M.', yes:'Yes', no:'', abstain:'Abstain'},
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+
+}
+
+export class SenatorVoteStatus {
+
+  name: string;
+  yes: string;
+  no: string;
+  abstain: string;
+
+}
